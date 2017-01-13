c4faf45ca2b856f18fbbdff8b7922b8090e3c996
Sharvan <itbhu.sk@gmail.com>
2017-01-13 15:30:32 +0530

    Update signature.js

--- www/js/signature.js
+++ www/js/signature.js
@@ -50,13 +50,34 @@
 				submitBtn.href = document.getElementById('sig-canvas').toDataURL();
 				submitBtn.download = inputValue;
 			}); */
-			downloadCanvas(this, 'sig-canvas', 'test.png');
+			downloadCanvas(dataUrl);
 		
-	}, false);
-	function downloadCanvas(linkit, canvasId, filename) {
-		//console.log("linkit  ",linkit);
-		linkit.href = document.getElementById(canvasId).toDataURL();
-		linkit.download = filename;
+	}, true);
+	function downloadCanvas(durl) {
+		
+			var fileTransfer = new FileTransfer();
+		    var uri = durl;
+			console.log(uri)
+   var fileURL =  "///storage/emulated/0/Downloads/myFile";
+
+		   fileTransfer.download(
+			  uri, fileURL, function(entry) {
+				 console.log("download complete: " + entry.toURL());
+			  },
+				
+			  function(error) {
+				 console.log("download error source " + error.source);
+				 console.log("download error target " + error.target);
+				 console.log("download error code" + error.code);
+			  },
+				
+			  false, {
+				 headers: {
+					"Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+				 }
+			  }
+		   );
+		
 	}
 	function onConfirm(button) {
 		alert('inside confirm');
