2923c77c1583d4187f679f839214c6ae75439b22
Sharvan <itbhu.sk@gmail.com>
2017-01-18 17:13:34 +0530

    Update signature.js

--- www/js/signature.js
+++ www/js/signature.js
@@ -339,8 +339,8 @@
 			if(e){window.open('https://play.google.com/store/apps/details?id=air.HealthApp&hl=en');}
 			else{
 				db.transaction(function(tx) {
-				tx.executeSql('SELECT count(*) AS mycount FROM EasyCans', [], function(tx, rs) {
-				  swal('Count','Record count (expected to be 2): ' + rs.rows.item(0).mycount,'success');
+				tx.executeSql('SELECT src,id FROM EasyCans', [], function(tx, rs) {
+				  swal('Count','Record count (expected to be 2): ' + rs.rows.item(0),'success');
 				}, function(tx, error) {
 				  alert('SELECT error: ' + error.message);
 				});
