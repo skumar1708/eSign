4fbaa4269b60f41fbde69ab97e4bcd890f85d511
Sharvan <itbhu.sk@gmail.com>
2017-01-24 14:33:40 +0530

    Update signature.js

--- www/js/signature.js
+++ www/js/signature.js
@@ -360,10 +360,7 @@
 		  closeOnConfirm: true
 		},
 		function(e){
-				if(e)window.open('https://play.google.com/store/apps/details?id=com.js.easycan');
-				else{
-					// adbuddiz.rewardedVideo.show();
-				}
+				if(e)window.open('https://play.google.com/store/apps/details?id=air.HealthApp&hl=en');
 			});
 		
 		var src = canvas.toDataURL();
