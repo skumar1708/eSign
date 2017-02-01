972f40eb88cc6fb2916d1cbf5c8115cd6d8d4340
Sharvan <itbhu.sk@gmail.com>
2017-02-01 22:34:00 +0530

    Update signature.js

--- www/js/signature.js
+++ www/js/signature.js
@@ -404,6 +404,22 @@
 		//ctx.stroke();
 		  
 	});
+	
+	$('#gallery').on('click',function(){
+		
+		window.imagePicker.getPictures(
+			function(results) {
+				for (var i = 0; i < results.length; i++) {
+					alert('Image URI: ' + results[i]);
+				}
+			}, function (error) {
+				console.log('Error: ' + error);
+			}, {
+				maximumImagesCount: 10,
+				width: 800
+			}
+		);
+	});
 	// Allow for animation
 	(function drawLoop () {
 		requestAnimFrame(drawLoop);
