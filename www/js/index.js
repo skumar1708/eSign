0503a8ceb3adb2f21cd1924a1cd99f21b0f9fc79
Sharvan <itbhu.sk@gmail.com>
2017-10-26 20:39:42 +0530

    after-admob

--- www/js/index.js
+++ www/js/index.js
@@ -16,13 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- var CANVAS_GLOBAL_URL = "";
-  var CANVAS_GLOBAL_LAST_URL = "";
- var androidApplicationLicenseKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyWuTJQClo0zEUXsxqBtAfj1BeUUF6lvpNG6zzzHJcss3YWK6XUuegy/eUvJgDO0L1yY1xhcC/beyGXEW/is7Ua5DMPx+IzqUFFtrx/xp32c5JB27p6XajqsvqkVXBV76UQLnaO5afNVR4gQquuIS72MceH9D5x4nvbZOh1vtEBVhmCoXMXeZ4VvISQ3gxGuNXnMl/p0sWM6gb5qfN5YjLCGTU8BhpazBQnqu4dqGLlLokpMGGkMogh8/LrF7dSZfbeSsZcZkY9h15gsDzlc41lMGgbQ5EkKUbHxCQQKfdfrsFJEFiD0cmRC9jB9bu47M+p8F8lYIiAu3ICeH0WkmMwIDAQAB";
+var CANVAS_GLOBAL_URL = "";
+var CANVAS_GLOBAL_LAST_URL = "";
+var androidApplicationLicenseKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyWuTJQClo0zEUXsxqBtAfj1BeUUF6lvpNG6zzzHJcss3YWK6XUuegy/eUvJgDO0L1yY1xhcC/beyGXEW/is7Ua5DMPx+IzqUFFtrx/xp32c5JB27p6XajqsvqkVXBV76UQLnaO5afNVR4gQquuIS72MceH9D5x4nvbZOh1vtEBVhmCoXMXeZ4VvISQ3gxGuNXnMl/p0sWM6gb5qfN5YjLCGTU8BhpazBQnqu4dqGLlLokpMGGkMogh8/LrF7dSZfbeSsZcZkY9h15gsDzlc41lMGgbQ5EkKUbHxCQQKfdfrsFJEFiD0cmRC9jB9bu47M+p8F8lYIiAu3ICeH0WkmMwIDAQAB";
 var productIds = "product_easy_export";
 var existing_purchases = [];
 var product_info = {};
- var publisherKey = "889e674c-6a74-4491-874a-90e8550561ae";
+var publisherKey = "889e674c-6a74-4491-874a-90e8550561ae";
 var app = {
     // Application Constructor
     initialize: function() {
